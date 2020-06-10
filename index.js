9a013198712d5bdc53dbc1e9e747703a9998f9cd
Alex Palomes <alejandropalomes@hotmail.com>
2020-06-11 00:47:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ public/script.js
@@ -0,0 +1,13 @@
+console.log("jhello");
+ $(function () {
+        var socket = io();
+        $('form').submit(function (e) {
+            e.preventDefault(); // prevents page reloading
+            socket.emit('chat message', $('#m').val());
+            $('#m').val('');
+            return false;
+        });
+        socket.on('chat message', function (msg) {
+            $('#messages').append($('<li>').text(msg));
+        });
+    });
